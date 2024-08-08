28685c60eb0314b4a1dc31b28d4ea359905b1371
H.Taeuk <hwoogiboogi1129@gmail.com>
2024-08-08 21:13:10 +0900

    Update k8s_auto_install_master.sh
    bash-completion 추가
    코드 최적화

--- k8s_auto_install_master.sh
+++ k8s_auto_install_master.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
-#1. system upgrade
-echo "System Upgrade..."
-sudo apt-get update && sudo apt-get upgrade -y
+# 중단 시 에러 발생
+set -e
 
-#2. Kubernetes 설정
+# 1. 시스템 업데이트 및 필수 패키지 설치
+echo "System Upgrade and Install Required Packages..."
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl gpg apt-transport-https
+
+# 2. Kubernetes 설정
 echo "Kubernetes Configure..."
 cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
 net.ipv4.ip_forward = 1
 EOF
 sudo sysctl --system
 
-#3. Docker Install
+# 3. Docker 설치
 echo "Docker Install..."
-sudo apt-get update -y
-sudo apt-get install -y ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -23,46 +25,49 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update -y
 
+sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-#4. Kubernetes 설치
+# 4. Kubernetes 설치
 echo "Kubernetes Install..."
-sudo apt-get update -y
-sudo apt-get install -y apt-transport-https ca-certificates curl gpg
-
-mkdir /etc/apt/keyrings
 curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-
 echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
-#5. kubeadm, kubelet, kubectl Install
+# 5. kubeadm, kubelet, kubectl 설치
 echo "Kubeadm, Kubelet, Kubectl Install..."
-sudo apt-get update -y
+sudo apt-get update
 sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
 sudo systemctl enable --now kubelet
 
-#6. CGroup Configure
+# 6. CGroup 설정
 echo "Cgroup Configuring..."
 sudo mkdir -p /etc/containerd
 containerd config default | sudo tee /etc/containerd/config.toml
 sudo sed -i 's/SystemdCgroup = false/SystemdCgroup = true/g' /etc/containerd/config.toml
 sudo systemctl restart containerd
 
-#7. swap-off
-echo "swap off Set..."
+# 7. Swap 비활성화
+echo "Swap off Set..."
 sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
 sudo swapoff -a
 
-#8. Master Node Setting
+# 8. Master Node 설정
 echo "Master Node Setting..."
 kubeadm init --pod-network-cidr=10.244.0.0/16
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
-#9. CNI Install
+# 9. CNI 설치
 echo "CNI Install..."
 kubectl apply -f https://github.com/flannel-io/flannel/releases/latest/download/kube-flannel.yml
+
+# 10. Bash Completion 설치
+source /usr/share/bash-completion/bash_completion
+echo 'source <(kubectl completion bash)' >>~/.bashrc
+echo 'alias k=kubectl' >>~/.bashrc
+echo 'complete -F __start_kubectl k' >>~/.bashrc
+
+echo "Kubernetes setup completed successfully!"
