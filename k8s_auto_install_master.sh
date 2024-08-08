823152c3e6f6098a5e3d7d58606abc5af0720805
H.Taeuk <hwoogiboogi1129@gmail.com>
2024-08-08 21:58:16 +0900

    Update k8s_auto_install_master.sh

--- k8s_auto_install_master.sh
+++ k8s_auto_install_master.sh
@@ -65,9 +65,10 @@ echo "CNI Install..."
 kubectl apply -f https://github.com/flannel-io/flannel/releases/latest/download/kube-flannel.yml
 
 # 10. Bash Completion 설치
+apt install bash-completion
 source /usr/share/bash-completion/bash_completion
 echo 'source <(kubectl completion bash)' >>~/.bashrc
 echo 'alias k=kubectl' >>~/.bashrc
 echo 'complete -F __start_kubectl k' >>~/.bashrc
 
-echo "Kubernetes setup completed successfully!"
+echo "Kubernetes setup completed successfully! Please restart shell."
