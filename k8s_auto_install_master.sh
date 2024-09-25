b4a1797f598f80a5c0c6c3f4f5160c34bb077217
H.Taeuk <hwoogiboogi1129@gmail.com>
2024-09-26 00:32:01 +0900

    Update k8s_auto_install_master.sh
    Bug Fix

--- k8s_auto_install_master.sh
+++ k8s_auto_install_master.sh
@@ -9,10 +9,22 @@ sudo apt-get update
 sudo apt-get install -y ca-certificates curl gpg apt-transport-https
 
 # 2. Kubernetes 설정
-echo "Kubernetes Configure..."
-cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
-net.ipv4.ip_forward = 1
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+overlay
+br_netfilter
 EOF
+
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+# sysctl params required by setup, params persist across reboots
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward                 = 1
+EOF
+
+# Apply sysctl params without reboot
 sudo sysctl --system
 
 # 3. Docker 설치
